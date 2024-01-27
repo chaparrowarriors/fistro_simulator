8c5ecdb32466b7c55095fb1da4fa061b1a381cf1
Miguel Angel Cano Mesa <miguelangelcanomesa@gmail.com>
2024-01-27 17:44:19 +0100

    commit baile

--- Scenes/Main/Eventos/Baile/baile.gd
+++ Scenes/Main/Eventos/Baile/baile.gd
@@ -1,102 +1,145 @@
 extends Node2D
 
-var secuen_max = 4 # Máximo de inputs necesarios para superar el Baile
-var tiempo_max = 5.0 # Segundos máximos para realizar la secuencia.
-var ran = RandomNumberGenerator.new()
-var random_max = 4 # Variable aleatoria del máximo de opciones disponibles.
+var secuen_max = 10  # Máximo de inputs necesarios para superar el Baile
+var tiempo_max = 10.0 # Segundos máximos para realizar la secuencia.
 
-var input # Valor del input esperado
-
-#Textruas
-var textura1 = load("res://Assets/Images/flecha_der.jpg")
-var textura2 = load("res://Assets/Images/flecha_izq.jpg")
-var textura3 = load("res://Assets/Images/flecha_up.jpg")
-var textura4 = load("res://Assets/Images/flecha_down.jpg")
-
-# Called when the node enters the scene tree for the first time.
+# Constantes de posición de pantalla x min/max y y min/max
+var viewport_tam
+var x_max
+var y_max
+var x_min = 80
+var y_min = 80
+var pos_x
+var pos_y
+	
 func _ready():
 	$Timer.wait_time = tiempo_max
 	$Timer.start()
 	
+	viewport_tam = get_viewport_rect().size
+	x_max = viewport_tam.x - 80
+	y_max = viewport_tam.y - 80 
 	
-	input = ran.randi_range(1, random_max)
-	match input:
-		1:
-			$qte_icon.texture = textura1
-		2:
-			$qte_icon.texture = textura2
-		3:
-			$qte_icon.texture = textura3
-		4:
-			$qte_icon.texture = textura4
-
-
+	generar_pulsador(1)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
 	pass
+
+func generar_pulsador(num):
 	
+	pos_x = randf_range( x_min, x_max)
+	pos_y = randf_range( y_min, y_max)
 	
-func _input(event):
-	var exito = false
-	
-	# Comprobamos si el input corresponde
-	if event.is_action_pressed("ui_right"):
-		if input == 1:
-			exito = true
-		else:
-			exito = false
-	elif event.is_action_pressed("ui_left"):
-		if input == 2:
-			exito = true
-		else:
-			exito = false	
-	elif event.is_action_pressed("ui_up"):
-		if input == 3:
-			exito = true
-		else:
-			exito = false	
-	elif event.is_action_pressed("ui_down"):
-		if input == 4:
-			exito = true
-		else:
-			exito = false
-	else:
-		return
-	
-	# Cargamos el siguiente input si la secuencia aún no hubiera terminado.	
-	if exito == true:	
-		input = ran.randi_range(1, random_max)
-		secuen_max = secuen_max - 1
-		
-		$qte_icon.texture = null
-		await get_tree().create_timer(0.1).timeout
-		if secuen_max > 0:
-			match input:
-				1:
-					$qte_icon.texture = textura1
-				2:
-					$qte_icon.texture = textura2
-				3:
-					$qte_icon.texture = textura3
-				4:
-					$qte_icon.texture = textura4
-		else:
-			fin_baile(exito)
-	else:
-		fin_baile(exito)
-		
+	match num:
+		1:
+			$qte1.set_position(Vector2(pos_x, pos_y))
+			$qte1.visible = true
+		2:
+			$qte2.set_position(Vector2(pos_x, pos_y))
+			$qte2.visible = true
+		3:
+			$qte3.set_position(Vector2(pos_x, pos_y))
+			$qte3.visible = true
+		4:
+			$qte4.set_position(Vector2(pos_x, pos_y))
+			$qte4.visible = true
+		5:
+			$qte5.set_position(Vector2(pos_x, pos_y))
+			$qte5.visible = true
+		6:
+			$qte6.set_position(Vector2(pos_x, pos_y))
+			$qte6.visible = true
+		7:
+			$qte7.set_position(Vector2(pos_x, pos_y))
+			$qte7.visible = true
+		8:
+			$qte8.set_position(Vector2(pos_x, pos_y))
+			$qte8.visible = true
+		9:
+			$qte9.set_position(Vector2(pos_x, pos_y))
+			$qte9.visible = true
+		10:
+			$qte10.set_position(Vector2(pos_x, pos_y))
+			$qte10.visible = true
+			
+func _on_qte_1_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte1.visible = false
+			secuen_max = secuen_max - 1
+			generar_pulsador(2)
+			
+func _on_qte_2_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte2.visible = false
+			secuen_max = secuen_max - 1
+			generar_pulsador(3)
+
+func _on_qte_3_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte3.visible = false
+			secuen_max = secuen_max - 1
+			generar_pulsador(4)
+
+
+func _on_qte_4_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte4.visible = false
+			secuen_max = secuen_max - 1
+			generar_pulsador(5)
+
+
+func _on_qte_5_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte5.visible = false
+			secuen_max = secuen_max - 1
+			generar_pulsador(6)
+
+
+func _on_qte_6_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte6.visible = false
+			secuen_max = secuen_max - 1
+			generar_pulsador(7)
+
+
+func _on_qte_7_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte7.visible = false
+			secuen_max = secuen_max - 1
+			generar_pulsador(8)
+
+
+func _on_qte_8_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte8.visible = false
+			secuen_max = secuen_max - 1
+			generar_pulsador(9)
+
+
+func _on_qte_9_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte9.visible = false
+			secuen_max = secuen_max - 1
+			generar_pulsador(10)
+
+
+func _on_qte_10_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT: 
+			$qte10.visible = false
+			fin_baile(true)
+			
 func fin_baile(qte_result):
-	Global.chiste_result = qte_result
+	Global.baile_result = qte_result
 	
 	if qte_result == true:
 		print("EXITO")
 	else:
 		print("FALLO")
 		
-	get_tree().change_scene_to_file("res://Scenes/stage.tscn")
-	
+	get_tree().change_scene_to_file("res://Scenes/Main/Stage.tscn")
 
 func _on_timer_timeout():
 	print("TIMEOUT")
-	Global.chiste_result = false
-	get_tree().change_scene_to_file("res://Scenes/stage.tscn")
+	Global.baile_result = false
+	get_tree().change_scene_to_file("res://Scenes/Main/Stage.tscn")
