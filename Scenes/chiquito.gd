16d2ded2f169ec9a5a973f37b69d77453ed5cf4d
Raul Alvarez <ralvarez@granadev.es>
2024-01-27 11:48:03 +0100

    Add Chiquito character and stage scene

--- Scenes/chiquito.gd
+++ Scenes/chiquito.gd
@@ -2,13 +2,18 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -200.0
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+func _ready():
+	$AnimatedSprite2D.play('default')
+
 func _physics_process(delta):
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -20,6 +25,11 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = 1
+	else:
+		$AnimatedSprite2D.flip_h = 0
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
