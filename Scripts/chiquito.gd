38680cad1bf9a7c9e6d5e1e6f05a30f9e0c77e4d
Raul Alvarez <ralvarez@granadev.es>
2024-01-27 13:47:38 +0100

    Add new images and update stage script

--- Scripts/chiquito.gd
+++ Scripts/chiquito.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -200.0
+var chisteOn = false
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var iconos = ["Bocadillo/Icono1", "Bocadillo/Icono2", "Bocadillo/Icono3", "Bocadillo/Icono4"]
 
 func _ready():
 	$AnimatedSprite2D.play('default')
@@ -19,16 +21,13 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction < 0:
-		$AnimatedSprite2D.flip_h = 1
-	if direction > 0:
-		$AnimatedSprite2D.flip_h = 0
+	changeDir(direction)
 		
 	if direction:
 		velocity.x = direction * SPEED
@@ -36,3 +35,29 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func showChiste(chiste):
+	if chisteOn:
+		return
+	
+	chisteOn = true
+	
+	chiste -= 1
+	get_node(iconos[chiste]).visible = 1
+	$Bocadillo.visible = 1
+	await get_tree().create_timer(3).timeout
+	$Bocadillo.visible = 0
+	get_node(iconos[chiste]).visible = 0
+	
+	chisteOn = false
+
+
+func changeDir(direction):
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = 1
+		$Bocadillo.flip_h = 1
+		$Bocadillo.position.x = -60
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = 0
+		$Bocadillo.flip_h = 0
+		$Bocadillo.position.x = 60
