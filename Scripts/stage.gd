53b69a9d0b83f9840b2d5ae495b46d934ee61c8b
Miguel Angel Cano Mesa <miguelangelcanomesa@gmail.com>
2024-01-28 15:36:55 +0100

    ITS THE FINAL COMMIT TINONINO

--- Scripts/stage.gd
+++ Scripts/stage.gd
@@ -16,7 +16,6 @@ func _ready():
 	$TimerFoco.wait_time = tiempo_foco
 	$TimerFoco.start()
 	$TimerFinFoco.wait_time = tiempo_vida_foco
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
